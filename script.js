f4756ae450179ca16afa08feec3cdf0ba9ee2071
Sun-G0D <47151235+Sun-G0D@users.noreply.github.com>
2024-02-13 07:30:05 -0800

    big changes

---
+++ script.js
@@ -0,0 +1,24 @@
+if (history.scrollRestoration) {
+  history.scrollRestoration = 'manual';
+} else {
+  window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+  }
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const timeSinceKnown = new Date(2023, 6, 21)
+const timeSinceDate = new Date(2023, 8, 9, 2, 31)
